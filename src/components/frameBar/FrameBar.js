24c494cb9bc52cc0d97ff4b2ea848f75d306d911
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-01-16 20:02:26 +0300

    feat: add grid layout

--- src/components/frameBar/FrameBar.js
+++ src/components/frameBar/FrameBar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class FrameBar extends Component {
     render() {
         return (
-            <div></div>
+            <div className='frame-bar'></div>
         )
     }
 }
--- src/components/preview/Preview.js
+++ src/components/preview/Preview.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Preview extends Component {
     render() {
         return (
-            <div></div>
+            <div className='prview'></div>
         )
     }
 }
