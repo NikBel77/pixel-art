13ac311066b5e8e87913273a32761169cea3552e
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-01-16 19:10:56 +0300

    feat: create basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './main.css'
+import FrameBar from './components/frameBar/FrameBar'
+import ToolBar from './components/toolBar/ToolBar'
+import SideBar from './components/sideBar/SideBar'
+import Header from './components/header/Header'
+import Painter from './components/painter/Painter'
+import Preview from './components/preview/Preview'
 
-function App() {
-  return (
-    <div className='app grey lighten-2'></div>
-  );
+class App extends Component{
+  render() {
+    return (
+    <div className='app grey lighten-2'>
+      <Header />
+      <ToolBar />
+      <Painter />
+      <FrameBar />
+      <Preview />
+      <SideBar />
+    </div>
+    )}
 }
 
 export default App;
---
+++ src/components/painter/Painter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Painter extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Painter
