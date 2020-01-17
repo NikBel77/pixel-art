eb86aae54e2321cac72943e30e9ed60fd6529124
Nikita Belyakov <nikita.belyakov.38@ya.ru>
2020-01-17 15:31:20 +0300

    feat: add canvas style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import './header.css'
 
 class Header extends Component {
     render() {
         return (
-            <div className='header'></div>
+            <div className='header indigo z-depth-2'>
+                <h1 className='header__title'>
+                    <span style={{color: '#f4ff81'}}>Piskel </span>
+                    <span style={{color: '#ffc107'}}>Art</span>
+                </h1>
+            </div>
         )
     }
 }
--- src/components/painter/Painter.js
+++ src/components/painter/Painter.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react'
+import './painter.css'
 
 class Painter extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            canvasSize: {
+                width: 512,
+                height: 512,
+                scale: 16,
+            },
+            coords: {
+                x: 0,
+                y: 0,
+            }
+        }
+    }
+
+    updateCoords(e) {
+        const [x, y] = this.getCoordsFromOffset(e.offsetX, e.offsetY);
+        [this.refs.fildX.innerText, this.refs.fildY.innerText] = [x, y];
+
+        if(this.state.coords.x !== x || this.state.coords.y !== y) {
+            this.setState(({ coords }) => {
+                coords.x = x;
+                coords.y = y;
+            });
+
+            this.refs.shadow.style.top = `${y * this.state.canvasSize.scale}px`;
+            this.refs.shadow.style.left = `${x * this.state.canvasSize.scale}px`;
+        }
+    }
+
+    getCoordsFromOffset(offsetX, offsetY) {
+        const x = Math.floor(offsetX / this.state.canvasSize.scale);
+        const y = Math.floor(offsetY / this.state.canvasSize.scale);
+        return [x >= 0 ? x : 0, y >= 0 ? y : 0];
+    }
+
     render() {
         return (
-            <div className='painter'></div>
+            <div className='painter'>
+                <div className='painter__inner z-depth-4'
+                    style={{width: `${this.state.canvasSize.width}px`,
+                        height: `${this.state.canvasSize.height}px`}}
+                    onMouseEnter={() => { this.refs.shadow.style.display = 'Block' }}
+                    onMouseLeave={() => { this.refs.shadow.style.display = 'none' }}>
+                    <canvas className='canvas'
+                        style={{width: `${this.state.canvasSize.width}px`,
+                            height: `${this.state.canvasSize.height}px`}}
+                        onMouseMove={(e) => { this.updateCoords(e.nativeEvent) }}>
+                    </canvas>
+                    <div className='painter__coords'>
+                        x : <span ref='fildY'>0</span> y : <span ref='fildX'>0</span>
+                    </div>
+                    <div className='painter__shadow'
+                        style={{width: `${this.state.canvasSize.scale}px`,
+                            height: `${this.state.canvasSize.scale}px`}}
+                        ref='shadow'
+                    ></div>
+                </div>
+            </div>
         )
     }
 }
